5f1fb7e559d7531cb765dc9c05f3e08ce719c7f2
Nicolas-Miguel <Nicolas-Miguel@test.com>
2022-03-28 10:27:18 -0300

    Desenvolvimento do exercicio 6.

---
+++ src/ExercicioSeis.java
@@ -0,0 +1,19 @@
+
+public class ExercicioSeis {
+	public static void main(String[] args) {
+		Double peso = 5.5;
+		
+		if(peso < 4) {
+			System.out.println("Abaixo do peso");
+		
+		}
+		else if (peso >10) {
+			System.out.println("Acima do peso");
+	   }
+		else if(peso >= 4 && peso <=10) {
+			System.out.println("Peso Normal");
+		}
+	}
+	
+
+}
