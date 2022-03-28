b89547178bc296b03207e3d018a00ecd1d146e0d
Nicolas-Miguel <Nicolas-Miguel@test.com>
2022-03-28 10:17:25 -0300

    Desenvolvimento do exercicio 5

---
+++ src/ExercicioCinco.java
@@ -0,0 +1,11 @@
+
+public class ExercicioCinco {
+	public static void main(String[] args) {
+		double nota1 = 8.0 , nota2 = 7.0, nota3 = 10, nota4 = 9.5;
+		
+		Double Med = (nota1 + nota2 + nota3 + nota4) / 4;
+		
+		 System.out.println(" " + Med);
+		
+	}
+}
